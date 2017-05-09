39b59595a1519559f9c61df926e97a288675ac5d
emersonprado <emerson.prado.eng@gmail.com>
2017-05-09 12:16:57 -0300

    Parameterize execution user and group in defined type make

--- manifests/make.pp
+++ manifests/make.pp
@@ -16,6 +16,8 @@ define gnu_build_system::make (
   $path,
   $opts    = [],
   $target  = undef,
+  $user    = undef,
+  $group   = undef,
 ) {
 
   $command = join(concat(['make'], $opts, $target), ' ')
@@ -23,6 +25,8 @@ define gnu_build_system::make (
   exec { "${path}/${command}":
     command => $command,
     cwd     => $path,
+    user    => $user,
+    group   => $group,
     path    => ['/bin','/usr/bin'],
   }
 
